6f09fb1ca2543c1be268e542966af58df1ab7754
valerius <valerius@mbteknologi.com>
2023-09-13 18:51:41 +0700

    improvement icon

--- resources/views/website/base.blade.php
+++ resources/views/website/base.blade.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Company Bootstrap Template - Index</title>
+    <title>Koperasi Konsumen-MBS</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
-    <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link href="{{asset('icon-kps.png')}}" rel="icon">
+    <link href="{{asset('icon-kps.png')}}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link
